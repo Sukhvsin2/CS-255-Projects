89da7e57555378e96dfb8b472ad316ba77c6fc5c
Sukhvsin2 <46042060+Sukhvsin2@users.noreply.github.com>
2021-10-21 12:37:22 -0500

    Some functions Added

--- Project 2/Poly.cpp
+++ Project 2/Poly.cpp
@@ -15,3 +15,41 @@ istream& operator>>(istream &is,Poly &obj){
 	for(int i=0;i<MAX;i++) is>>obj.terms[i];
 	return is;
 }
+
+void Poly:: Reset(){
+	for(int i=0;i<MAX;i++){
+		terms[i] = 0;
+	}
+}
+
+double Poly:: Eval(double x){
+	double temp = 1;
+	for(int i=0;i<x;i++) temp *= x;
+	return temp;
+}
+
+Poly operator+(const Poly& p1, const Poly& p2){
+	Poly result;
+	for(int i=0;i<MAX;i++){
+		result.terms[i] = p1.terms[i] + p2.terms[i];
+	}
+	return result;
+}
+
+Poly operator*(const Poly& p1, const Poly& p2){
+	Poly result;
+	for(int i=0;i<MAX;i++){
+		result.terms[i] = p1.terms[i] * p2.terms[i];
+	}
+	return result;
+}
+
+void Poly:: operator+=(const Poly& p){
+	for(int i=0;i<MAX;i++){
+		terms[i] += p.terms[i];
+	}
+}
+
+void Poly:: Derivative(){
+	
+}
